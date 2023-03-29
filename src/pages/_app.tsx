37295ab77f9f2508795f57573b2d798d4fd94795
jamesqquick <james.q.quick@gmail.com>
2023-03-29 15:34:30 -0500

    Added twitter side, description, and url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from 'next/app';
 import { UserProvider } from '@/hooks/User';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Head>
+        <meta name="twitter:card" content="photo" />
+        <meta name="twitter:site" content={process.env.NEXT_PUBLIC_APP_URL} />
+        <meta
+          name="twitter:description"
+          content="Generate memes of James Q Quick"
+        />
+        <meta name="twitter:url" content="https://jqqmemes.com/" />
+      </Head>
       <div className="min-h-screen flex flex-col justify-between">
         <div className="p-4 md:px-20 ">
           <div className="bg-white">
